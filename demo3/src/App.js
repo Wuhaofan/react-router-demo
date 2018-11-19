abda1f6a61549290c7ae8f080aba26eaa32678be
yuchanglong <yuchanglong@zhangyue.com>
2018-11-19 18:47:53 +0800

    完成link示例

--- demo3/src/App.js
+++ demo3/src/App.js
@@ -12,15 +12,14 @@ class App extends Component {
     return (
       <div className="App">
         <BrowserRouter
-            basename='/web'
             forceRefresh={supportsHistory} // false: 不刷新 true: 刷新
             keyLength={12} // location.key长度为12
         >
           <Switch> 
-            <Route component={Home} path='/home' exact /> // 实际路径 /web/home
-            <Route component={Content} path='/content' /> // 实际路径 /web/page
-            <Route component={Log} path='/log' />  // 实际路径 /web/log
-            <Route component={Other} path='/other' /> // 实际路径 /web/other
+            <Route component={Home} path='/' exact /> 
+            <Route component={Content} path='/content' />
+            <Route component={Log} path='/log' /> 
+            <Route component={Other} path='/other' />
           </Switch>
         </BrowserRouter>
       </div>
--- demo3/src/containers/Nav/Nav.js
+++ demo3/src/containers/Nav/Nav.js
@@ -4,38 +4,42 @@ import './Nav.css'
 
 export default class Nav extends Component {
     render() {
+        const refCallback = node => {
+            // node在未渲染的时候是null，在渲染的时候是真实dom
+            console.log(node)
+        }
         return (
-          <div className='flex nav'>
-            <div className='flex-item nav-item'>
-                <Link to='/home'>
-                主页
-                </Link>
-            </div>
-            <div className='flex-item'>
-                <Link to={
-                    {
-                        pathname: '/content',
-                        search: 'hello',
-                        hash: 'world',
-                        state: {
-                            name: '测试员1'
+            <div className='flex nav'>
+                <div className='flex-item nav-item'>
+                    <Link to='/'>
+                    主页
+                    </Link>
+                </div>
+                <div className='flex-item'>
+                    <Link to={
+                        {
+                            pathname: '/content',
+                            search: 'hello',
+                            hash: 'world',
+                            state: {
+                                name: '测试员1'
+                            }
                         }
-                    }
-                }>
-                内容
-                </Link>
+                    }>
+                    内容
+                    </Link>
+                </div>
+                <div className='flex-item'>
+                    <Link replace to = '/log'>
+                    日志
+                    </Link>
+                </div>
+                <div className='flex-item'>
+                    <Link to='/other' innerRef={refCallback}>
+                    其他
+                    </Link>
+                </div>
             </div>
-            <div className='flex-item'>
-                <Link to='/log'>
-                日志
-                </Link>
-            </div>
-            <div className='flex-item'>
-                <Link to='/other'>
-                其他
-                </Link>
-            </div>
-          </div>
         )
     }
 }
